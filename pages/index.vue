d6bcbb7438d273b4d286f322cd7b3f0f26f25de7
Brian Lawrence <Brian@BrianDLawrence.com>
2023-10-13 15:48:30 +0200

    Initial

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="flex justify-center py-10">
+      <div class="prose lg:prose-xl">
+        <h1>Career Advisor</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: "Cats and Poetry is Catetry",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Unleash your inner poet with Catetry.com! Craft whimsical, heartwarming poems for your feline friends, and celebrate the quirky charms that make them purr-fectly unique.",
+    },
+  ],
+});
+</script>
